ef9345f3c8b0fae394d6d237801e572aff570f44
Edmund Noble <edmundnoble@gmail.com>
2016-12-03 05:57:19 -0500

    Fix concat tests

--- bench/src/test/scala/scolls/SeqBenchmarks.scala
+++ bench/src/test/scala/scolls/SeqBenchmarks.scala
@@ -1,7 +1,9 @@
 package scolls
 
+import java.nio.ByteBuffer
+
 import org.scalameter.api._
-import org.scalameter.picklers.IntPickler
+import org.scalameter.picklers.{IntPickler, PrimitivePickler}
 
 import scala.annotation.tailrec
 
@@ -12,14 +14,35 @@ object SeqBenchmarks
 
   implicit val intPickler = IntPickler
 
+  implicit val twoIntPickler = new PrimitivePickler[(Int, Int)] {
+    override protected def bits: Int = java.lang.Integer.SIZE * 2
+    override protected def unwrap(from: ByteBuffer): (Int, Int) = (from.getInt(), from.getInt())
+    override def pickle(x: (Int, Int)): Array[Byte] = byteBuffer.putInt(x._1).putInt(x._2).array()
+  }
+
   val constructSizes: Gen[Int] = Gen.enumeration("sizes")(10, 100, 1000, 2000)
   val destructSizes: Gen[Int] = Gen.enumeration("sizes")(100, 1000)
+  val concatInnerOuterSizes: Gen[(Int, Int)] = Gen.enumeration("sizes")((100, 10))
   val treeSizes: Gen[Int] = Gen.enumeration("treeSizes")(100, 500, 1200)
   val balancedTreeSizes: Gen[Int] = Gen.enumeration("treeSizes")(15, 20)
   val leftNestedTrees: Gen[LTree[Int]] = treeSizes.map(generateLeftNestedTree)
   val rightNestedTrees: Gen[LTree[Int]] = treeSizes.map(generateRightNestedTree)
   val jaggedNestedTrees: Gen[LTree[Int]] = treeSizes.map(generateJaggedTree)
   val balancedNestedTrees: Gen[LTree[Int]] = balancedTreeSizes.map(generateBalancedTree)
+  def consStructsToConcat[S[_]](implicit S: Sequence[S]): Gen[List[S[Int]]] =
+    for {
+      innerOuterSizes <- concatInnerOuterSizes
+      (innerSize, outerSize) = innerOuterSizes
+      innerSeq = consRecBench[S](innerSize)
+      outerSeq = List.fill(outerSize)(innerSeq)
+    } yield outerSeq
+  def snocStructsToConcat[S[_]](implicit S: Sequence[S]): Gen[List[S[Int]]] =
+    for {
+      innerOuterSizes <- concatInnerOuterSizes
+      (innerSize, outerSize) = innerOuterSizes
+      innerSeq = snocRecBench[S](innerSize)
+      outerSeq = List.fill(outerSize)(innerSeq)
+    } yield outerSeq
 
   def generateLeftNestedTree(size: Int): LTree[Int] = {
     @tailrec def generate(acc: LTree[Int], cnt: Int): LTree[Int] = {
@@ -96,22 +119,22 @@ object SeqBenchmarks
   def snocRecBench[S[_]](size: Int)(implicit S: Sequence[S]): S[Int] = {
     @annotation.tailrec
     def loop(s: S[Int], cnt: Int): S[Int] = {
-      if (cnt == 0) {
+      if (cnt == 0)
         s
-      } else {
+       else
         loop(S.snoc(s, cnt), cnt - 1)
-      }
     }
     loop(S.empty[Int], size)
   }
 
-  def sumBench[S[_]](seq: S[Int])(implicit S: Sequence[S]): Int = {
+  def sumBench[S[_]](seq: S[Int])(implicit S: Sequence[S]): Int =
     S.fold(seq)(0)(_ + _)
-  }
 
-  def concatBench[S[_], A](seq: List[S[A]])(implicit S: CSequence[S]): S[A] = {
+  def concatBenchRight[S[_], A](seq: List[S[A]])(implicit S: CSequence[S]): S[A] =
     seq.reduceRight(S.concat)
-  }
+
+  def concatBenchLeft[S[_], A](seq: List[S[A]])(implicit S: CSequence[S]): S[A] =
+    seq.reduceLeft(S.concat)
 
   def frontierCPS[S[_], A](tree: LTree[A])(implicit S: Sequence[S]): S[A] = {
     def helper(innerTree: LTree[A]): S[A] => S[A] = innerTree match {
@@ -163,21 +186,37 @@ object SeqBenchmarks
     measure method "cat" in (using(testSnocSeqOnes[Catenable]) in sumBench[Catenable])
   }
 
-//  performance of "concatenating cons-constructed seqs" in {
-//    measure method "list" in (using(testConsSeqOnes[List]) in concatBench[List, Int])
-//    measure method "vec" in (using(testConsSeqOnes[Vector]) in concatBench[Vector, Int])
-//    measure method "que" in (using(testConsSeqOnes[OkasakiQueue]) in concatBench[OkasakiQueue, Int])
-//    measure method "hque" in (using(testConsSeqOnes[HQueue]) in concatBench[HQueue, Int])
-//    measure method "cat" in (using(testSnocSeqOnes[Catenable]) in concatBench[Catenable, Int])
-//  }
-//
-//  performance of "concatenating snoc-constructed seqs" in {
-//    measure method "list" in (using(testSnocSeqOnes[List]) in concatBench[List, Int])
-//    measure method "vec" in (using(testSnocSeqOnes[Vector]) in concatBench[Vector, Int])
-//    measure method "que" in (using(testSnocSeqOnes[OkasakiQueue]) in concatBench[OkasakiQueue, Int])
-//    measure method "hque" in (using(testSnocSeqOnes[HQueue]) in concatBench[HQueue, Int])
-//    measure method "cat" in (using(testSnocSeqOnes[Catenable]) in concatBench[Catenable, Int])
-//  }
+  performance of "concatenating cons-constructed seqs to the right" in {
+    measure method "list" in (using(consStructsToConcat[List]) in concatBenchRight[List, Int])
+    measure method "vec" in (using(consStructsToConcat[Vector]) in concatBenchRight[Vector, Int])
+    measure method "que" in (using(consStructsToConcat[OkasakiQueue]) in concatBenchRight[OkasakiQueue, Int])
+    measure method "hque" in (using(consStructsToConcat[HQueue]) in concatBenchRight[HQueue, Int])
+    measure method "cat" in (using(consStructsToConcat[Catenable]) in concatBenchRight[Catenable, Int])
+  }
+
+  performance of "concatenating snoc-constructed seqs to the right" in {
+    measure method "list" in (using(snocStructsToConcat[List]) in concatBenchRight[List, Int])
+    measure method "vec" in (using(snocStructsToConcat[Vector]) in concatBenchRight[Vector, Int])
+    measure method "que" in (using(snocStructsToConcat[OkasakiQueue]) in concatBenchRight[OkasakiQueue, Int])
+    measure method "hque" in (using(snocStructsToConcat[HQueue]) in concatBenchRight[HQueue, Int])
+    measure method "cat" in (using(snocStructsToConcat[Catenable]) in concatBenchRight[Catenable, Int])
+  }
+
+  performance of "concatenating cons-constructed seqs to the left" in {
+    measure method "list" in (using(consStructsToConcat[List]) in concatBenchLeft[List, Int])
+    measure method "vec" in (using(consStructsToConcat[Vector]) in concatBenchLeft[Vector, Int])
+    measure method "que" in (using(consStructsToConcat[OkasakiQueue]) in concatBenchLeft[OkasakiQueue, Int])
+    measure method "hque" in (using(consStructsToConcat[HQueue]) in concatBenchLeft[HQueue, Int])
+    measure method "cat" in (using(consStructsToConcat[Catenable]) in concatBenchLeft[Catenable, Int])
+  }
+
+  performance of "concatenating snoc-constructed seqs to the left" in {
+    measure method "list" in (using(snocStructsToConcat[List]) in concatBenchLeft[List, Int])
+    measure method "vec" in (using(snocStructsToConcat[Vector]) in concatBenchLeft[Vector, Int])
+    measure method "que" in (using(snocStructsToConcat[OkasakiQueue]) in concatBenchLeft[OkasakiQueue, Int])
+    measure method "hque" in (using(snocStructsToConcat[HQueue]) in concatBenchLeft[HQueue, Int])
+    measure method "cat" in (using(snocStructsToConcat[Catenable]) in concatBenchLeft[Catenable, Int])
+  }
 
   performance of "concatenating left-nested trees" in {
     measure method "list" in (using(leftNestedTrees) in frontierRec[List, Int])
